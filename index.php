261e94d7259a3245fd11226bb0364391df1214c9
Corey Rogers <crogers25@mail.greenriver.edu>
2020-05-18 17:56:07 -0400

    Added controller and validate classes and updated composer

--- index.php
+++ index.php
@@ -3,28 +3,37 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Start a session
-session_start();
-
 //Require the autoload file
 require_once('vendor/autoload.php');
-require_once('model/data-layer.php');
-require_once('model/validate.php');     //validation belongs in the model!  anything to do with data belongs in model
 
 //Require data-layer file
 require_once('model/data-layer.php');
+
+//require_once('model/validate.php');     //validation belongs in the model!  anything to do with data belongs in model
+//require_once('classes/order.php');  don't need this now b/c we updated composer
+
+//Start a session   ***!!!DO this after the autoload so the classes will be loaded!!!
+session_start();
+
+//$order = new Order("tacos", "burgers", ["beers"]);
+//echo $order->toString();
+
+
 
 //Instantiate the F3 Base class
 $f3 = Base::instance();   //Base is a class; instance() is a static method in the Base class
                           // this becomes the object we use to run fatfree
                           //PHP uses double colon to call static methods
 
+//Instantiate a Validate object
+$validator = new Validate;
+
+//Instantiate a Controller object
+$controller = new Controller($f3, $validator);
+
 //Define a default route
 $f3->route('GET /', function() {
-    //echo '<h1>Welcome to my Food Page</h1>';
-
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['controller']->home();
 });
 
 //Breakfast route
@@ -63,103 +72,17 @@ $f3->route('GET /lunch/turkey-panini', function() {
 });
 
 //Order route
-$f3->route('GET|POST /order', function($f3){      // |POST tells f3 it can access the order route via the Post method
+$f3->route('GET|POST /order', function(){      // |POST tells f3 it can access the order route via the Post method
                                                     // as well as the GET method.  And you can make a different route for
                                                     // POST /order if you want to.
-
-
-    //If the form has been submitted
-    if($_SERVER["REQUEST_METHOD"]=="POST") {
-        //var_dump($_POST);
-        //array(2) { ["food"]=> string(5) "tacos" ["meal"]=> string(5) "lunch" }
-
-        //validate food
-        if (!validFood($_POST['food'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set("errors['food']", "Invalid food item");
-
-        }
-        //validate meal
-        if (!validFood($_POST['meal'])) {
-
-            //Set an error variable in the F3 hive
-            $f3->set('errors["meal"]', "Invalid meal");
-
-        }
-
-        /*
-        if(empty($_POST['food']) || !in_array($_POST['meal'], $meals)) {
-            echo"<p>Please enter a food and select a meal</p>";
-        }
-
-        */
-
-        //data is valid
-        if(empty($f3->get('errors'))) {
-            //Store the data in the session array - b/c this form is posting to self, and you don't want to lose data
-            // when you go to the summary page
-            $_SESSION['food'] = $_POST['food'];
-            $_SESSION['meal'] = $_POST['meal'];
-
-            //Redirect to summary page
-            $f3->reroute('orderTwo');
-
-        }
-
-    }
-
-
-
-    //add meals array to hive
-    $f3->set('meals', getMeals());
-    $f3->set('food', $_POST['food']);
-    $f3->set('selectedMeal', $_POST['meal']);
-
-
-    $view = new Template();
-    echo $view->render("views/order.html");
-
+    $GLOBALS['controller']->order();
 
 });
 
 //Order2 route
-$f3->route('GET|POST /orderTwo', function($f3){
+$f3->route('GET|POST /orderTwo', function(){
 
-    //create a condiments array
-    $condiments = getCondiments();
-
-    //If the form has been submitted
-    if($_SERVER["REQUEST_METHOD"]=="POST") {
-        //var_dump($_POST);
-        //array(2) { ["food"]=> string(5) "tacos" ["meal"]=> string(5) "lunch" }
-
-        //validate the data
-        //echo"<p>Order form two data valid.</p>";
-
-
-        //data is valid - I haven't added validation yet
-
-
-        //Store the data in the session array - b/c this form is posting to self, and you don't want to lose data
-        // when you go to the summary page
-        $_SESSION['condiments'] = $_POST['condiments'];
-
-        //var_dump($_SESSION['condiments']);
-
-        //Redirect to summary page
-        $f3->reroute('summary');
-
-
-    }
-
-
-    //add meals array to hive
-    $f3->set('condiments', $condiments);
-
-    $view = new Template();
-    echo $view->render("views/order2.html");
-
+    $GLOBALS['controller']->orderTwo();
 
 });
 
@@ -167,14 +90,8 @@ $f3->route('GET|POST /orderTwo', function($f3){
 
 //Summary route
 $f3->route('GET /summary', function() {
-    //echo '<h1>Welcome to my summary</h1>';
 
-    $view = new Template();
-    echo $view->render('views/summary.html');
-
-    session_destroy();   //to make sure we're starting with a blank slate the next time someone comes to place an order
-    //any site shared on one domain shares the same session, so it's important to destroy the session when
-    // you are done with it so you don't run into old data or data from a different app.
+    $GLOBALS['controller']->summary();
 
 });
 
--- model/validate.php
+++ model/validate.php
@@ -1,40 +1,49 @@
 <?php
 
-//validate.php
-
-/*  Return a value indicating if food parameter is valid
-    Valid foods are not empty and do not contain anything except letters
-    @param String $food
-    @return boolean
-*/
-function validFood($food)
+/**
+ * Class Validate
+ * Contains the validatoin methods for my app
+ * @author Corey Rogers
+ * @version 1.0
+ */
+class Validate
 {
-    $food = str_replace(' ', '', $food);
-    return !empty($food) && ctype_alpha($food);
+
+    /*  Return a value indicating if food parameter is valid
+        Valid foods are not empty and do not contain anything except letters
+        @param String $food
+        @return boolean
+    */
+    function validFood($food)
+    {
+        $food = str_replace(' ', '', $food);
+        return !empty($food) && ctype_alpha($food);
+    }
+
+    /* for testing purposes only
+    echo validFood("french fries") ? "yes<br>" : "no<br>";
+    echo validFood("pizza") ? "yes<br>" : "no<br>";
+    echo validFood("7-layer dip") ? "yes<br>" : "no<br>";
+    echo validFood("") ? "yes<br>" : "no<br>";
+    */
+
+    /*  Return a value indicating if meal is valid
+        Valid meals are breakfast, lunch, and dinner
+        @param String $meal
+        @return boolean
+    */
+    function validMeal($meal)
+    {
+        $meals = getMeals();
+        return in_array($meal, $meals);
+    }
+
+    /*
+    //for testing purposes only
+    echo validMeal("breakfast") ? "yes<br>" : "no<br>";
+    echo validMeal("") ? "yes<br>" : "no<br>";
+    echo validMeal("dessert") ? "yes<br>" : "no<br>";
+    echo validMeal("lunch") ? "yes<br>" : "no<br>";
+    */
+
 }
-
-/* for testing purposes only
-echo validFood("french fries") ? "yes<br>" : "no<br>";
-echo validFood("pizza") ? "yes<br>" : "no<br>";
-echo validFood("7-layer dip") ? "yes<br>" : "no<br>";
-echo validFood("") ? "yes<br>" : "no<br>";
-*/
-
-/*  Return a value indicating if meal is valid
-    Valid meals are breakfast, lunch, and dinner
-    @param String $meal
-    @return boolean
-*/
-function validMeal($meal)
-{
-    $meals = getMeals();
-    return in_array($meal, $meals);
-}
-
-/*
-//for testing purposes only
-echo validMeal("breakfast") ? "yes<br>" : "no<br>";
-echo validMeal("") ? "yes<br>" : "no<br>";
-echo validMeal("dessert") ? "yes<br>" : "no<br>";
-echo validMeal("lunch") ? "yes<br>" : "no<br>";
-*/
